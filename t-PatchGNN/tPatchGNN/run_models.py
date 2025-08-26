80a646c37cc9fb47c2495e1177d1e82be19d8c4b
Indraneel N U <87777762+ind047@users.noreply.github.com>
2025-08-26 12:19:09 +0200

    Change default for use_attention to False

--- t-PatchGNN/tPatchGNN/run_models.py
+++ t-PatchGNN/tPatchGNN/run_models.py
@@ -85,7 +85,7 @@ parser.add_argument(
 parser.add_argument(
     "--use_attention",
     type=bool,
-    default=True,
+    default=False,
     help="Whether to use attention mechanism in the model.",
 )
 # value 0 means using original time granularity, Value 1 means quantization by 1 hour,
